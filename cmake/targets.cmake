ee3fbfa0450c3b01b0e596b3e22442fbeab4ae71
Cyril La Valle <cyril.la-valle@epitech.eu>
2017-10-21 16:48:18 +0200

    cmake: fixed target-specific flags catenation

--- cmake/targets.cmake
+++ cmake/targets.cmake
@@ -1,9 +1,7 @@
 # Debug target
 if(CMAKE_BUILD_TYPE STREQUAL "Debug")
   add_definitions(-DDEBUG)
-  add_compile_options(
-    -g -march=atom -msse -msse2 -msse3 -mssse3 -msse4.1 -msse4.2 -ffast-math
-    -ftree-loop-if-convert -funroll-loops -mfpmath=sse -DDEBUG)
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -g3 -DDEBUG")
   if(WITH_SANITIZERS)
     add_compile_options(
       -fno-optimize-sibling-calls -fno-omit-frame-pointer
@@ -14,6 +12,5 @@ endif()
 
 # Release target
 if(CMAKE_BUILD_TYPE STREQUAL "Release")
-  add_compile_options(-O3 -march=atom -msse -msse2 -msse3 -mssse3 -msse4.1 -msse4.2 -ffast-math
-    -ftree-loop-if-convert -funroll-loops -mfpmath=sse)
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O3 -ffast-math")
 endif()
