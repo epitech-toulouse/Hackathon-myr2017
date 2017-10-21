fef8c887e73978e57192833804a42ed9ce634135
Cyril La Valle <cyril.la-valle@epitech.eu>
2017-10-21 16:46:16 +0200

    misc: utilities used by Oz::Camera

---
+++ EpitechClient/utilities.hh
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <ApiCodec/ApiStereoCameraPacket.hpp>
+
+/*
+ * Says whether an ApiStereoCameraPacket contains zlib-compressed images.
+ */
+inline bool is_image_packet_zlib(const std::shared_ptr<ApiStereoCameraPacket> & packet)
+{
+	return (
+		packet->imageType == ApiStereoCameraPacket::ImageType::RAW_IMAGES_ZLIB ||
+        	packet->imageType == ApiStereoCameraPacket::ImageType::UNRECTIFIED_COLORIZED_IMAGES_ZLIB ||
+        	packet->imageType == ApiStereoCameraPacket::ImageType::RECTIFIED_COLORIZED_IMAGES_ZLIB
+	);
+}
+ 
+/*
+ * Says whether an ApiStereoCameraPacket contains bayer (aka. raw) images.
+ */
+inline bool is_image_packet_bayer(const std::shared_ptr<ApiStereoCameraPacket> & packet)
+{
+	return (
+		packet->imageType == ApiStereoCameraPacket::ImageType::RAW_IMAGES ||
+		packet->imageType == ApiStereoCameraPacket::ImageType::RAW_IMAGES_ZLIB
+	);
+}
+
+size_t zlib_uncompress(uint8_t * dest, const uint8_t * src, size_t dest_max_size, size_t src_size);
