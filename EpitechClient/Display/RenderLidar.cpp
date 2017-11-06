e1297fb907e0e2f8b0cc21f010a2706d84691f7c
Cyril La Valle <cyril.la-valle@epitech.eu>
2017-11-06 19:27:20 +0100

    display: lighten RenderLidar::draw routine

--- EpitechClient/Display/RenderLidar.cpp
+++ EpitechClient/Display/RenderLidar.cpp
@@ -5,8 +5,45 @@ namespace Display
 {
 
 RenderLidar::RenderLidar(void) :
-	_vertices { sf::Points, LIDAR_CAPTURE_RESOLUTION }
+	_background_color { 0x0F0100FF },
+	_grid_lines_color { 0x440B05FF },
+	_vertices { sf::Points, LIDAR_CAPTURE_RESOLUTION },
+	_background { sf::Triangles, 6 },
+	_angle_grid_lines { sf::Lines, 28 * 2 }
 {
+	/* Background */
+	double half = 1000; /* Background half size */
+	_background[0].position = sf::Vector2f(-half, -half);
+	_background[1].position = sf::Vector2f(-half, half);
+	_background[2].position = sf::Vector2f(half, -half);
+	_background[3].position = sf::Vector2f(half, half);
+	_background[4].position = sf::Vector2f(-half, half);
+	_background[5].position = sf::Vector2f(half, -half);
+	for (size_t i = 0 ; i < 6 ; ++i) {
+		_background[i].color = sf::Color(_background_color);
+	}
+
+	/* Grid */
+	double angle = LIDAR_BEGIN_ANGLE;
+	for (size_t idx = 0 ; idx < 28 * 2 ; ++idx) {
+		double x = 1000.0 * cos(angle * M_PI / 180.0);
+		double y = 1000.0 * sin(angle * M_PI / 180.0);
+		_angle_grid_lines[idx].position = sf::Vector2f(0, 0);
+		_angle_grid_lines[idx].color = sf::Color(_grid_lines_color);
+		++idx;
+		_angle_grid_lines[idx].position = sf::Vector2f(x, y);
+		_angle_grid_lines[idx].color = sf::Color(_grid_lines_color);
+		angle += 10.0;
+	}
+	float radius = 0.0;
+	for (auto && line : _radius_grid_lines) {
+		radius += 50.0;
+		line.setPosition(-radius, -radius);
+		line.setRadius(radius);
+		line.setFillColor(sf::Color(0));
+		line.setOutlineThickness(1);
+		line.setOutlineColor(sf::Color(_grid_lines_color));
+	}
 }
 
 void RenderLidar::update(const std::array<uint16_t, LIDAR_CAPTURE_RESOLUTION> & distances)
@@ -24,48 +61,11 @@ void RenderLidar::draw(sf::RenderTarget& target, sf::RenderStates states) const
 {
 	states.transform *= this->getTransform();
 	states.texture = NULL;
-
-	sf::VertexArray bg(sf::Triangles, 6);
-	double half = 1000;
-	bg[0].position = sf::Vector2f(-half, -half);
-	bg[1].position = sf::Vector2f(-half, half);
-	bg[2].position = sf::Vector2f(half, -half);
-	bg[3].position = sf::Vector2f(half, half);
-	bg[4].position = sf::Vector2f(-half, half);
-	bg[5].position = sf::Vector2f(half, -half);
-	for (size_t i = 0 ; i < 6 ; ++i) { bg[i].color = sf::Color(0x0F0100FF); }
-	target.draw(bg, states);
-
-	std::array<sf::CircleShape, 10> radius_lines;
-
-	float radius = 0.0;
-	for (auto && line : radius_lines) {
-		radius += 50.0;
-		line.setPosition(-radius, -radius);
-		line.setRadius(radius);
-		line.setFillColor(sf::Color(0));
-		line.setOutlineThickness(1);
-		line.setOutlineColor(sf::Color(0x440B05FF));
-	}
-
-	for (auto && line : radius_lines) {
+	target.draw(_background, states);
+	target.draw(_angle_grid_lines, states);
+	for (auto && line : _radius_grid_lines) {
 		target.draw(line, states);
 	}
-
-	sf::VertexArray angle_lines(sf::Lines, 28 * 2);
-	double angle = LIDAR_BEGIN_ANGLE;
-	for (size_t idx = 0 ; idx < 28 * 2 ; ++idx) {
-		double x = 1000.0 * cos(angle * M_PI / 180.0);
-		double y = 1000.0 * sin(angle * M_PI / 180.0);
-		angle_lines[idx].position = sf::Vector2f(0, 0);
-		angle_lines[idx].color = sf::Color(0x440B05FF);
-		++idx;
-		angle_lines[idx].position = sf::Vector2f(x, y);
-		angle_lines[idx].color = sf::Color(0x440B05FF);
-		angle += 10.0;
-	}
-	target.draw(angle_lines, states);
-
 	target.draw(_vertices, states);
 }
 
--- EpitechClient/Display/RenderLidar.hh
+++ EpitechClient/Display/RenderLidar.hh
@@ -19,7 +19,12 @@ private:
 	void draw(sf::RenderTarget& target, sf::RenderStates states) const;
 
 private:
+	sf::Color _background_color;
+	sf::Color _grid_lines_color;
 	sf::VertexArray _vertices;
+	sf::VertexArray _background;
+	sf::VertexArray _angle_grid_lines;
+	std::array<sf::CircleShape, 10> _radius_grid_lines;
 };
 
 }
