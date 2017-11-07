f6fd921179bd6f3ae5f4657e172caf1f95b6009f
Rodolphe Asséré <rodolphe1.assere@epitech.eu>
2017-11-07 10:11:10 +0100

    playground: move main to playground object

---
+++ EpitechClient/Playground/Playground.cpp
@@ -0,0 +1,66 @@
+#include "Playground/Playground.hh"
+
+namespace Playground
+{
+  Playground::Playground(const std::string& hostAddress, uint16_t hostMainPort, uint16_t hostCameraPort) :
+    gateway { hostAddress, hostMainPort, hostCameraPort },
+    oz { this->gateway },
+    display {}
+  {
+    /* Launch playground */
+    try {
+      this->run();
+    } catch (const ClientException & error) {
+      std::cerr << "fatal: " << error.what() << std::endl;
+    }
+  }
+
+  /* Extern function to launch thread */
+  static void lidar_noise(Display::Display & display, Gateway::Gateway & gateway)
+  {
+    std::array<uint16_t, LIDAR_CAPTURE_RESOLUTION> dist;
+    while (gateway.is_running()) {
+      for (auto & value : dist) {
+	value = static_cast<uint16_t>(float(random()) / float(RAND_MAX) * 400);
+      }
+      display.update_lidar(dist);
+      std::this_thread::sleep_for(WAIT_TIME_MS);
+    }
+  }
+
+  int Playground::run() {
+    this->display.set_target_camera(&(this->oz.getCamera()));
+    this->gateway.start();
+    this->oz.getCamera().start();
+    std::cout << "Waiting for connection..." << std::endl;
+    while (!this->oz.getCamera().is_running() || !this->gateway.is_running()) {
+      std::this_thread::sleep_for(WAIT_TIME_MS);
+    }
+    using Command = ApiCommandPacket::CommandType;
+    this->gateway.enqueue(std::make_unique<ApiCommandPacket>(Command::TURN_ON_IMAGE_ZLIB_COMPRESSION));
+    this->gateway.enqueue(std::make_unique<ApiCommandPacket>(Command::TURN_ON_API_RAW_STEREO_CAMERA_PACKET));
+    //    std::thread lidar_noise_thread = std::thread(this->lidar_noise, std::ref(this->display), std::ref(this->gateway));
+    std::thread lidar_noise_thread = std::thread(lidar_noise, std::ref(this->display), std::ref(this->gateway));
+    this->display.show();
+    this->oz.getCamera().stop();
+    this->gateway.stop();
+    lidar_noise_thread.join();
+    return 0;
+  }
+  
+
+  Oz::Oz& Playground::getOz(void)
+  {
+    return (oz);
+  }
+
+  Gateway::Gateway& Playground::getGateway(void)
+  {
+    return (gateway);
+  }
+
+  Display::Display& Playground::getDisplay(void)
+  {
+    return (display);
+  }
+}
--- EpitechClient/main.cpp
+++ EpitechClient/main.cpp
@@ -1,61 +1,6 @@
-#include <functional>
-#include <ApiCodec/ApiCommandPacket.hpp>
-#include "Gateway/Gateway.hh"
-#include "Oz/Oz.hh"
-#include "Display/Display.hh"
-#include "exceptions.hh"
-#include "constants.hh"
-#include <cstdlib>
-
-
-static void lidar_noise(Display::Display & display, Gateway::Gateway & gateway)
-{
-	std::array<uint16_t, LIDAR_CAPTURE_RESOLUTION> dist;
-	while (gateway.is_running()) {
-		for (auto & value : dist) {
-			value = static_cast<uint16_t>(float(random()) / float(RAND_MAX) * 400);
-		}
-		display.update_lidar(dist);
-		std::this_thread::sleep_for(WAIT_TIME_MS);
-	}
-}
-
-/*
- * TODO: This code must be moved to Playground class when implemented.
- */
-static int run(
-	Gateway::Gateway & gateway,
-	Oz::Camera & camera,
-	Display::Display & display
-) {
-	display.set_target_camera(&camera);
-	gateway.start();
-	camera.start();
-	std::cout << "Waiting for connection..." << std::endl;
-	while (!camera.is_running() || !gateway.is_running()) {
-		std::this_thread::sleep_for(WAIT_TIME_MS);
-	}
-	using Command = ApiCommandPacket::CommandType;
-	gateway.enqueue(std::make_unique<ApiCommandPacket>(Command::TURN_ON_IMAGE_ZLIB_COMPRESSION));
-	gateway.enqueue(std::make_unique<ApiCommandPacket>(Command::TURN_ON_API_RAW_STEREO_CAMERA_PACKET));
-	std::thread lidar_noise_thread = std::thread(lidar_noise, std::ref(display), std::ref(gateway));
-	display.show();
-	camera.stop();
-	gateway.stop();
-	lidar_noise_thread.join();
-	return 0;
-}
+#include "Playground/Playground.hh"
 
 int main(int, char **)
 {
-	Gateway::Gateway gateway("127.0.0.1", 5559, 5558);
-	Oz::Oz oz(gateway);
-	Display::Display display;
-
-	try {
-	  return run(gateway, oz.getCamera(), display);
-	} catch (const ClientException & error) {
-		std::cerr << "fatal: " << error.what() << std::endl;
-	}
-	return 1;
+  Playground::Playground playground("127.0.0.1", 5559, 5558);
 }
